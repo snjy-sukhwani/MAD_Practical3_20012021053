72467baca65bedf0867c338067cad956d4a21271
Sanjay Sukhwani <sanjaysukhwani20@gnu.ac.in>
2022-09-12 19:53:26 +0530

    Changing Kotlin File and using Binding in it

--- app/src/main/java/com/example/mad_practical3_20012021053/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical3_20012021053/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.mad_practical3_20012021053
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
+import com.example.mad_practical3_20012021053.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar (binding.toolbar)
     }
 }
