8ba4a862093b5d63f44a75a9c6b09e9efc64709a
Sanjay Sukhwani <sanjaysukhwani20@gnu.ac.in>
2022-09-13 18:51:10 +0530

    Adding Code for Login Activity

--- app/src/main/java/com/example/mad_practical3_20012021053/LoginActivity.kt
+++ app/src/main/java/com/example/mad_practical3_20012021053/LoginActivity.kt
@@ -2,10 +2,19 @@ package com.example.mad_practical3_20012021053
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
+import com.example.mad_practical3_20012021053.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
     }
 }
