5d732129d30dcaea11f127b2d456ffdeda42bc0b
Sanjay Sukhwani <sanjaysukhwani20@gnu.ac.in>
2022-09-14 09:38:33 +0530

    Adding kotlin code for Login Activity

--- app/src/main/java/com/example/mad_practical3_20012021053/LoginActivity.kt
+++ app/src/main/java/com/example/mad_practical3_20012021053/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mad_practical3_20012021053
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.WindowCompat
@@ -16,5 +17,16 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
+        binding.BottomNavigationView.id = R.id.bottom_nav_registration
+        binding.BottomNavigationView.setOnItemSelectedListener { it2 ->
+            when (it2.itemId) {
+                R.id.bottom_nav_registration -> {
+                    Intent(this, RegistrationActivity::class.java).also { startActivity(it) }
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+
     }
 }
