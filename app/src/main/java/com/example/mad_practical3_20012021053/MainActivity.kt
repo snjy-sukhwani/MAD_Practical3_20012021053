8c06c83bae3857f7aa3ff82736bd4de85e7565dd
Sanjay Sukhwani <sanjaysukhwani20@gnu.ac.in>
2022-09-14 09:38:54 +0530

    Adding kotlin code for Main Activity

--- app/src/main/java/com/example/mad_practical3_20012021053/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical3_20012021053/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.mad_practical3_20012021053
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.view.WindowCompat
 import com.example.mad_practical3_20012021053.databinding.ActivityMainBinding
 
@@ -19,5 +21,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setSupportActionBar (binding.toolbar)
+
+        binding.BottomNavigationView.id = R.id.bottom_nav_registration
+        binding.BottomNavigationView.setOnItemSelectedListener { it2 ->
+            when(it2.itemId) {
+                R.id.bottom_nav_registration -> {
+                    Intent(this, RegistrationActivity::class.java).also { startActivity(it) }
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+
+        binding.BottomNavigationView.id = R.id.bottom_nav_login
+        binding.BottomNavigationView.setOnItemSelectedListener { it1 ->
+            when (it1.itemId) {
+                R.id.bottom_nav_login -> {
+                    Intent(this, LoginActivity::class.java).also { startActivity(it) }
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 }
