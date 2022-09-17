0d5708fe2b1947e1bac094b378606b486376dab3
Sanjay Sukhwani <sanjaysukhwani20@gnu.ac.in>
2022-09-17 08:58:53 +0530

    Update : Added Code for Fields Verification

--- app/src/main/java/com/example/mad_practical3_20012021053/RegistrationActivity.kt
+++ app/src/main/java/com/example/mad_practical3_20012021053/RegistrationActivity.kt
@@ -3,8 +3,11 @@ package com.example.mad_practical3_20012021053
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.text.isDigitsOnly
 import androidx.core.view.WindowCompat
 
 import com.example.mad_practical3_20012021053.databinding.ActivityRegistrationBinding
@@ -46,8 +49,62 @@ class RegistrationActivity : AppCompatActivity() {
             Toast.makeText(this,"Login Link is Clicked", Toast.LENGTH_LONG).show()
 
             // Code for Opening the Login/Signin Activity
-            var loginScreen = Intent(this,LoginActivity::class.java)
+            val loginScreen = Intent(this,LoginActivity::class.java)
             startActivity(loginScreen)
+        }
+
+        // Code for Checking Username & Password :
+        val registerBtnClicked = findViewById<Button>(R.id.btnRegister)
+
+        registerBtnClicked.setOnClickListener {
+
+            val username = findViewById<EditText>(R.id.regUsernameEditText).text.toString()
+            val phone = findViewById<EditText>(R.id.regPhoneEditText).text.toString()
+            val city = findViewById<EditText>(R.id.regCityEditText).text.toString()
+            val email = findViewById<EditText>(R.id.regEmailEditText).text.toString()
+            val password = findViewById<EditText>(R.id.regPasswordEditText).text.toString()
+            val confirmpass = findViewById<EditText>(R.id.regconfirmpasswordEditText).text.toString()
+
+
+            // Testing
+            if(username == "" || phone == "" || city == "" || email == "" || password == "" || confirmpass == "")
+            {
+                Toast.makeText(this,"Please Fill out the Fields !!",Toast.LENGTH_LONG).show()
+
+                val registerEmptyFields = Intent(this,RegistrationActivity::class.java)
+                startActivity(registerEmptyFields)
+            }
+
+            else if(password != confirmpass)
+            {
+                Toast.makeText(this,"Registration Failed!!\nPassword Should be Same in Both Fields",Toast.LENGTH_LONG).show()
+
+                val passwordMatchError = Intent(this,RegistrationActivity::class.java)
+                startActivity(passwordMatchError)
+            }
+
+            else if(phone.length != 10){
+
+                if(!phone.isDigitsOnly()){
+                    Toast.makeText(this,"Registration Failed!!\nPhone Number should contain digits Only",Toast.LENGTH_LONG).show()
+
+                    val phoneNotHavingDigits = Intent(this,RegistrationActivity::class.java)
+                    startActivity(phoneNotHavingDigits)
+                }
+                else {
+                    Toast.makeText(this,"Registration Failed!!\nPhone Number should have 10 digits Only",Toast.LENGTH_LONG).show()
+
+                    val phoneLengthError = Intent(this, RegistrationActivity::class.java)
+                    startActivity(phoneLengthError)
+                }
+            }
+
+            else{
+                Toast.makeText(this,"Registration Success!!",Toast.LENGTH_LONG).show()
+
+                val registerSuccess = Intent(this,RegistrationActivity::class.java)
+                startActivity(registerSuccess)
+            }
         }
     }
 }
